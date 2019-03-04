e72c796ae5db27f365d5b4d0d8d493afff5d81f2
Stephen Reiser <StephenReiser@Stephens-MacBook-Pro.local>
2019-03-04 17:56:51 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,26 +21,37 @@ var cardsInPlay = [];
 
 //checks to see if there is a match with first two cards
 var checkForMatch = function () {
-    //this top line is excluded from GA guide
-    //if (cardsInPlay.length === 2) {
+    if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry, try again.");
-    }
-}
-//}
+    };
+};
+};
 
-var flipCard = function (cardID) {
+var flipCard = function () {
+    var cardID = this.getAttribute("data-id");
     console.log("User flipped " + cards[cardID].rank);
     console.log("User flipped " + cards[cardID].suit);
     console.log("User flipped " + cards[cardID].cardImage);
     cardsInPlay.push(cards[cardID].rank);
+    //is this wehre this part goes? maybe because checkForMatch is the If statement.  From the instructions. this is wrong.  need to use this?
+    this.setAttribute('src',cards[cardID].cardImage);
     checkForMatch();
 }
-flipCard(0);
-flipCard(2);
-
+//added from the DOM section
+var createBoard = function () {
+    for (i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        //not sure if img and src need to be in double quotes ranter than single
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById("game-board").appendChild(cardElement);
+    }
+};
  
 
+createBoard();
 
